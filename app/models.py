80e64c4eb97b0c18c441342aea8226a3a2d80d75
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-06-07 15:22:40 -0400

    Update user model & add user methods.

--- app/models.py
+++ app/models.py
@@ -1,13 +1,16 @@
 from app import db, login
 from flask_login import UserMixin, current_user
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
 
+# Table to link users and assets
 user_holdings = db.Table('user_holdings',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('asset_id', db.Integer, db.ForeignKey('asset.id'))
 )
 
+# Table to link users and leagues
 user_leagues = db.Table('user_leagues',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('league_id', db.Integer, db.ForeignKey('league.id'))
@@ -22,8 +25,10 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String)
     avatar = db.Column(db.String)
     wins = db.Column(db.Integer, default=0)
-    bank = db.Column(db.Numeric(15,2))
+    bank = db.Column(db.Numeric(15,2), default=10000)
     created_on = db.Column(db.DateTime, default=dt.utcnow)
+    token = db.Column(db.String, unique=True, index=True)
+    token_exp = db.Column(db.DateTime)
     holdings = db.relationship(
         'Asset',
         secondary = user_holdings,
@@ -36,6 +41,52 @@ class User(UserMixin, db.Model):
         backref = 'users',
         lazy = 'dynamic'
     )
+
+    def __repr__(self):
+        return f'<User email: {self.email} | User ID: {self.id}>'
+
+    def __str__(self):
+        return f'<User email: {self.email} | User name: {self.first_name} {self.last_name}>'
+
+    # Salt and hash password
+    def hash_password(self, created_password):
+        return generate_password_hash(created_password)
+
+    # Check password submitted at login with hashed password in database
+    def confirm_password(self, login_password):
+        return check_password_hash(self.password, login_password)
+
+    # Set user info based on registration
+    def reg_to_db(self, reg_data):
+        self.first_name = reg_data['first_name'].lower().strip()
+        self.last_name = reg_data['last_name'].lower().strip()
+        self.display_name = reg_data['display_name'].lower().strip()
+        self.email = reg_data['email'].lower().strip()
+        self.password = self.hash_password(reg_data['password'])
+        self.avatar = reg_data['avatar']
+
+    # Save/update user info to database
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    # Get token upon login for token auth
+    def get_token(self, exp=24):
+        current_time = dt.utcnow()
+        if self.token and self.token_exp > current_time + timedelta(seconds=60):
+            return self.token
+        self.token = secrets.token_urlsafe(32)
+        self.token_exp = current_time + timedelta(hours=exp)
+        self.save()
+        return self.token
+
+    # Check if user has token and if token is expired
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if not user or user.token_exp < dt.utcnow():
+            return None
+        return user
 
 class League(db.Model):
     id = db.Column(db.Integer, primary_key=True)
