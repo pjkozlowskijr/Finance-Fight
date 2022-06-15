0a8d35125c9e94f4d69a235a79c051ec76972df9
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-06-15 11:23:55 -0400

    Reorganize files.

--- app/blueprints/asset/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('asset', __name__, url_prefix='')
-
-from . import asset_routes
--- app/blueprints/user/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('user', __name__, url_prefix='')
-
-from . import user_routes
