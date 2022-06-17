7bf68eb23e5773ec23fc70bae8c48f1e58848508
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-06-17 02:44:58 -0400

    Add ticker tape to view.

--- react-finance-fight/src/index.js
+++ react-finance-fight/src/index.js
@@ -7,6 +7,7 @@ import AppContextProvider from './context/AppContext';
 import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <AppContextProvider>
