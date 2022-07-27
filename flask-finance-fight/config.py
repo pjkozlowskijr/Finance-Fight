365a2c33236bc0599b2dab223f2804914beebee4
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-07-27 14:38:39 -0400

    Cleanup for deployment.

--- flask-finance-fight/config.py
+++ flask-finance-fight/config.py
@@ -1,7 +1,6 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
